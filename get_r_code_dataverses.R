a9b6a3f95ade84d3a8c2eb5fe6e28970c710b381
Christopher Chen <chrischen@uber.com>
2017-12-04 03:45:14 -0500

    assembled components to download R-file-containing dataverses

--- get_r_code_dataverses.R
+++ get_r_code_dataverses.R
@@ -3,16 +3,27 @@ library(dataverse)
 Sys.setenv("DATAVERSE_KEY" = "670994aa-dbf5-4240-a3a6-74cca05a9f07")
 Sys.setenv("DATAVERSE_SERVER" = "dataverse.harvard.edu")
 
-# 
-
 # trying out a dataverse search for all .R files
 rfiles = dataverse_search("*.R" ,type="file")
 names(rfiles)
-# useful part of each search result, containing doi
-rfiles[1,]$dataset_citation
 
-# get the number of R files to capture
-captured_output = capture.output(query_result = dataverse_search("*.R" ,type="file")$name, type="message")
+
+# trying out a dataverse search for .R files
+rfiles = dataverse_search("*.R", type="file")
+names(rfiles)
+
+# We can get any page of results with the "start" parameter (0-indexed). 
+# This should generate the same result as the above line
+rfiles1 = dataverse_search("*.R", type="file", start=0)
+setdiff(rfiles, rfiles1) # has no output if they are truly equal
+
+# we want to record the number of .R files so we know how many pages of metadata to receive
+# first we need to capture Y from the console message "X of Y results retrieved"
+captured_message = capture.output(query_result = dataverse_search("*.R" ,type="file")$name, type="message")
+
+# use regex (with stringr module) to parse out the number of files and convert to integer
+num_files = as.numeric(str_extract_all(captured_message, "[0-9]+")[[1]][2])
+num_files
 
 # query dataverse for .R files
 for (i in c(1:length(rfiles[,1]))) {
