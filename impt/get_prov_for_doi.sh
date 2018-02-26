71fb3d461f8515c9da91b0f00ffde21591748402
Christopher Chen <cscn@rclogin09.rc.fas.harvard.edu>
2018-02-25 20:59:58 -0500

    updated odyssey script to put stdout and stderror in special directory

--- impt/get_prov_for_doi.sh
+++ impt/get_prov_for_doi.sh
@@ -4,8 +4,8 @@
 #SBATCH -t 0-01:00              # Runtime in D-HH:MM
 #SBATCH -p serial_requeue      	# Partition to submit to
 #SBATCH --mem-per-cpu=1000  # Memory pool for all cores (see also --mem-per-cpu)
-#SBATCH -o provR%j.out      # File to which STDERR will be written
-#SBATCH -e provR%j.err      # File to which STDERR will be written
+#SBATCH -o ./logs/provR%j.out      # File to which STDERR will be written
+#SBATCH -e ./logs/provR%j.err      # File to which STDERR will be written
 
 # echo "Downloading dataset corresponding to DOI: $1..."
 # doi_direct=$(python download_dataset.py $1 $2)
