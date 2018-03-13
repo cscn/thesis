adf119b2980c899d8ba5a214d6bdb05b3a87bd9a
Christopher Chen <chrischen@uber.com>
2018-03-13 05:02:46 -0400

    shell script to speed up data aggregation on odyssey

---
+++ impt/aggregate_run_data.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH -n 8                    # Number of cores
+#SBATCH -N 1                    # Ensure that all cores are on one machine
+#SBATCH -t 0-01:00              # Runtime in D-HH:MM
+#SBATCH -p serial_requeue      	# Partition to submit to
+#SBATCH --mem-per-cpu=1000  # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH -o ./logs/provR%j.out      # File to which STDERR will be written
+#SBATCH -e ./logs/provR%j.err      # File to which STDERR will be written
+
+python aggregate_run_data.py $1 $2
