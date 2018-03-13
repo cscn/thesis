d9b0310d7406495077b380e9b090aea8a90bf552
Christopher Chen <chrischen@uber.com>
2018-03-13 10:39:14 -0400

    increased time limit to 4 hours on odyssey

--- impt/get_prov_for_doi.sh
+++ impt/get_prov_for_doi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -n 8                    # Number of cores
 #SBATCH -N 1                    # Ensure that all cores are on one machine
-#SBATCH -t 0-01:00              # Runtime in D-HH:MM
+#SBATCH -t 0-04:00              # Runtime in D-HH:MM
 #SBATCH -p serial_requeue      	# Partition to submit to
 #SBATCH --mem-per-cpu=1000  # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH -o ./logs/provR%j.out      # File to which STDERR will be written
