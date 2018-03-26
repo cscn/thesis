1304201109632df10e5437f13915dbdb8cf0c91b
Christopher Chen <chrischen@uber.com>
2018-03-26 05:05:17 -0400

    fixing careless mistake

--- impt/naive_preprocess.py
+++ impt/naive_preprocess.py
@@ -5,8 +5,6 @@ from helpers import convert_r_files, all_preproc
 # get the directory name as command line argument
 doi_direct = sys.argv[len(sys.argv) - 1]
 
-# read in the run log
-run_log = pd.read_csv(doi_direct + "/prov_data/run_log.csv")
 # get filenames to preprocess
 orig_files = [my_file for my_file in os.listdir(doi_direct) if\
 			  my_file.endswith(".R") or my_file.endswith(".r")]
